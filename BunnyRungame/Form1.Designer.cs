11701795767d289a2dff8e2a3a84aae0dd165aea
saada <mahrukhazhar3@gmail.com>
2024-12-25 14:18:47 +0500

    Add project files.

---
+++ BunnyRungame/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace BunnyRungame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblscore = new System.Windows.Forms.Label();
+            this.lblhighscore = new System.Windows.Forms.Label();
+            this.gametimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.Color.Transparent;
+            this.Player.Image = global::BunnyRungame.Properties.Resources.gameRabbit;
+            this.Player.Location = new System.Drawing.Point(101, 322);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(115, 112);
+            this.Player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Player.TabIndex = 0;
+            this.Player.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::BunnyRungame.Properties.Resources.gameobs;
+            this.pictureBox1.Location = new System.Drawing.Point(727, 356);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(107, 78);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "obstacle";
+            // 
+            // lblscore
+            // 
+            this.lblscore.AutoSize = true;
+            this.lblscore.BackColor = System.Drawing.Color.Transparent;
+            this.lblscore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblscore.Location = new System.Drawing.Point(21, 9);
+            this.lblscore.Name = "lblscore";
+            this.lblscore.Size = new System.Drawing.Size(119, 32);
+            this.lblscore.TabIndex = 2;
+            this.lblscore.Text = "Score:0";
+            // 
+            // lblhighscore
+            // 
+            this.lblhighscore.AutoSize = true;
+            this.lblhighscore.BackColor = System.Drawing.Color.Transparent;
+            this.lblhighscore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblhighscore.Location = new System.Drawing.Point(820, 9);
+            this.lblhighscore.Name = "lblhighscore";
+            this.lblhighscore.Size = new System.Drawing.Size(182, 32);
+            this.lblhighscore.TabIndex = 3;
+            this.lblhighscore.Text = "HighScore:0";
+            // 
+            // gametimer
+            // 
+            this.gametimer.Enabled = true;
+            this.gametimer.Interval = 20;
+            this.gametimer.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::BunnyRungame.Properties.Resources.gmbg;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1014, 520);
+            this.Controls.Add(this.lblhighscore);
+            this.Controls.Add(this.lblscore);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Player);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "BunnyRun";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblscore;
+        private System.Windows.Forms.Label lblhighscore;
+        private System.Windows.Forms.Timer gametimer;
+    }
+}
+
---
+++ BunnyRungame/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BunnyRungame
+{
+    public partial class Form1 : Form
+    {
+        int gravity;
+        int gravityValue = 8;
+        int obstaclespeed = 15;
+        int score = 0;
+        int highscore = 0;
+        bool gameOver = false;
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            RestartGame();
+          
+        }
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space && !gameOver)
+            {
+                Jump();
+            }
+            if (e.KeyCode == Keys.Enter && gameOver == true)
+            {
+                RestartGame();
+            }
+
+        }
+        private void Jump()
+        {            
+                gravity = -gravityValue; // Apply an upward force  
+        }
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+            
+            lblscore.Text = "Score:" +score;
+            lblhighscore.Text = "highscore:" + highscore;
+            Player.Top += gravity;
+            gravity += 1;
+            pictureBox1.Left -= obstaclespeed;
+            
+            
+
+            if (Player.Bottom >= 290) // Check if the player is on or slightly above the ground
+            {
+                Player.Top = 290 - Player.Height;
+                gravity = 0; // Reset gravity when the player is on the ground
+            }
+            if (pictureBox1.Left < -100)
+            {
+                pictureBox1.Left = random.Next(1200, 3000);
+                score += 1;
+            }
+
+            if (pictureBox1.Bounds.IntersectsWith(Player.Bounds) )
+            {
+                gametimer.Stop();
+                Player.Image = Properties.Resources.deadgm_removebg_preview;
+                
+                lblscore.Text += "Game Over !! press enter to restart.";
+                gameOver = true;
+                if(score> highscore)
+                {
+                    highscore = score;
+                }
+            }
+            if(score>10)
+            {
+                obstaclespeed = 30;
+                gravityValue = 15;
+            }
+        }
+        private void RestartGame()
+        {
+            Player.Location = new Point(101, 290);
+            Player.Image = Properties.Resources.gameRabbit;
+            score = 0;
+            gravityValue = 8;
+            gravity = gravityValue;
+            obstaclespeed = 15;
+            pictureBox1.Left = random.Next(1200, 3000);
+            gametimer.Start();
+            gameOver = false;
+            
+        }
+    }
+}
